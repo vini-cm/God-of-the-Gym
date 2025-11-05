b430b794da1c05ef10851a3191ed16fa77b53bba
Camila Schmidt <programmer666loser@gmail.com>
2025-11-05 08:03:13 -0300

    data export banco de dados atualizado

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: godofthegym
 -- ------------------------------------------------------
--- Server version	8.0.43
+-- Server version	8.4.4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -116,37 +116,6 @@ INSERT INTO `clientes` VALUES (3,'80212571990',78,1.78,15,24.6181,'Intermediario
 UNLOCK TABLES;
 
 --
--- Table structure for table `instrutores`
---
-
-DROP TABLE IF EXISTS `instrutores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `instrutores` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `salario` decimal(10,2) DEFAULT NULL,
-  `formacao` varchar(250) DEFAULT NULL,
-  `entrada` time DEFAULT NULL,
-  `saida` time DEFAULT NULL,
-  `cpf` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cpf` (`cpf`),
-  CONSTRAINT `instrutores_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `usuarios` (`cpf`),
-  CONSTRAINT `instrutores_ibfk_2` FOREIGN KEY (`cpf`) REFERENCES `usuarios` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `instrutores`
---
-
-LOCK TABLES `instrutores` WRITE;
-/*!40000 ALTER TABLE `instrutores` DISABLE KEYS */;
-INSERT INTO `instrutores` VALUES (2,1990.00,'EDUCAÇÃO FISICA - FURB','12:00:00','13:00:00','12345678910'),(4,1690.00,'EDUCAÇÃO FISICA ','11:30:00','15:30:00','44522009909');
-/*!40000 ALTER TABLE `instrutores` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `planos`
 --
 
@@ -160,7 +129,7 @@ CREATE TABLE `planos` (
   `nome` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome_UNIQUE` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +138,7 @@ CREATE TABLE `planos` (
 
 LOCK TABLES `planos` WRITE;
 /*!40000 ALTER TABLE `planos` DISABLE KEYS */;
-INSERT INTO `planos` VALUES (1,'Bimestral',450,'Gold');
+INSERT INTO `planos` VALUES (1,'Bimestral',450,'Gold'),(2,'Semestral',225,'Silver'),(3,'Trimestral',112.5,'Bronze'),(4,'Mensal',85,'Basic');
 /*!40000 ALTER TABLE `planos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,6 +150,7 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `cpf` varchar(11) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `sobrenome` varchar(45) NOT NULL,
@@ -190,10 +160,10 @@ CREATE TABLE `usuarios` (
   `senha` varchar(45) NOT NULL,
   `tipo` varchar(45) NOT NULL,
   `telefone` varchar(45) NOT NULL,
-  PRIMARY KEY (`cpf`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `CPF_UNIQUE` (`cpf`),
   UNIQUE KEY `Email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +172,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES ('03274063053','Bruno','Mars','1999-03-12','mars@gmail.com','f','f38fef4c0e4988792723c29a0bd3ca98','atendente','49984784224'),('12345678910','Stefanie','Miller','1990-10-31','ster1990@gmail.com','f','af21d0c97db2e27e13572cbf59eb343d','instrutor','4798134678'),('15004161933','Camila','Schmidt','2006-11-05','camila@gmail.com','f','ea5a486c712a91e48443cd802642223d','administrador','47998430987'),('44522009909','Joaquim Geraldo','Emanuel da Rosa','1988-05-23','joaquim_geraldo-rosa@gmail.com','m','827ccb0eea8a706c4c34a16891f84e7b','instrutor','4838752710'),('50333387902','Gabriel Daniel','Alves','2004-10-14','gabriel_daniel_alves@gmail.com','m','81dc9bdb52d04dc20036dbd8313ed055','atendente','47937293098'),('80212571990','Sueli Vanessa ','Lara Fogaça','1976-07-13','sueli_fogaca@hotmail.com','f','827ccb0eea8a706c4c34a16891f84e7b','cliente','47927627722');
+INSERT INTO `usuarios` VALUES (1,'15004161933','Camila','Schmidt','2006-11-05','camila@gmail.com','f','ea5a486c712a91e48443cd802642223d','administrador','47998430987'),(2,'12345678910','Stefanie','Miller','1990-10-31','ster1990@gmail.com','f','af21d0c97db2e27e13572cbf59eb343d','instrutor','4798134678'),(3,'03274063053','Bruno','Mars','1999-03-12','mars@gmail.com','f','f38fef4c0e4988792723c29a0bd3ca98','atendente','49984784224'),(8,'58846150007','Pandora','Padra','1995-04-12','padraPandora@gmail.com','f','372b89b73646c46842b8ab7555923579','cliente','49989417965');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -215,4 +185,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-02 20:54:41
+-- Dump completed on 2025-11-05  8:02:43
