7e5fe64f8fd27787a4fda5f0accfdf8f68cc0bb7
Camila <programmer666loser@gmail.com>
2025-11-11 18:02:45 -0300

    estilizar alertas

--- godOfGym/src/main/java/util/Alerta.java
+++ godOfGym/src/main/java/util/Alerta.java
@@ -1,42 +1,64 @@
 package util;
 
+import java.io.File;
 import java.util.Optional;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Alerta {
   private static Scene cenaAlerta;
-  public static void setCena(Scene scene){
-     cenaAlerta = scene;
- }
+  
+    public static void setCena(Scene scene){
+       cenaAlerta = scene;
+    }
+  
+    public static void aplicarEstilo(Alert alerta){
+        DialogPane dp = alerta.getDialogPane();
+        dp.getStylesheets().add(Alerta.class.getResource("/css/alertas.css").toExternalForm());
+    }
  
-   public static void mostrarAlerta(Alert.AlertType alertat, String titulo, String mgs) {
+    public static void mostrarAlerta(Alert.AlertType alertat, String titulo, String mgs, Image img) {
         Alert alerta = new Alert(alertat);
         alerta.setTitle(titulo);
         alerta.setHeaderText(null);
         alerta.setContentText(mgs);
+        aplicarEstilo(alerta);
+        ImageView imgV = new ImageView(img);
+        alerta.setGraphic(imgV);
         alerta.showAndWait();
     }
    
    public static void mostrarErro(String titulo, String mensagem){
-        mostrarAlerta(Alert.AlertType.ERROR, titulo, mensagem);
+       File file = new File("src/main/resources/imagens/alerta/erro.png");
+       Image img = new Image(file.toURI().toString());
+       mostrarAlerta(Alert.AlertType.ERROR, titulo, mensagem,img);
     }
     
     public static void mostrarInformacao(String titulo, String mensagem){
-        mostrarAlerta(Alert.AlertType.INFORMATION, titulo, mensagem);
+        File file = new File("src/main/resources/imagens/alerta/info.png");
+        Image img = new Image(file.toURI().toString());
+        mostrarAlerta(Alert.AlertType.INFORMATION, titulo, mensagem,img);
     }
     
     public static void mostrarAviso(String titulo, String mensagem){
-        mostrarAlerta(Alert.AlertType.WARNING, titulo, mensagem);
+        File file = new File("src/main/resources/imagens/alerta/aviso.png");
+        Image img = new Image(file.toURI().toString());
+        mostrarAlerta(Alert.AlertType.WARNING, titulo, mensagem,img);
     }
     
-    public static Optional<ButtonType> mostrarConfirmacao(String titulo,
-            String mensagem){
+    public static Optional<ButtonType> mostrarConfirmacao(String titulo,String mensagem){
         Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
         alerta.setTitle(titulo);
         alerta.setHeaderText(null);
         alerta.setContentText(mensagem);
+        File arquivo = new File("src/main/resources/imagens/alerta/conf.png");
+        Image img = new Image(arquivo.toURI().toString());
+        ImageView imgV = new ImageView(img);
+        aplicarEstilo(alerta);
         return alerta.showAndWait();
     }
         
